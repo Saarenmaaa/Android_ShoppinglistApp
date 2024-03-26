e86d6395128a30c51597b29418c9d5355335b40d
Olli Saarenmaa <o.saarenmaa@gmail.com>
2024-03-26 11:56:57 +0200

    Initial commit after paper installation

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
+
+
+
+//AppRegistry.registerComponent(appName, () => App);
